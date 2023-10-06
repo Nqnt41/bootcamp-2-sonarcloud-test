16507396bae750af3000fc9c5fe5c4a6bf8ddbf0
Nqnt41 <covenyr@ufl.edu>
2023-10-06 17:44:35 -0400

    Committing to new repo so that I can do SonarCloud observations on it.

--- JSONtoPostgreSQL.js
+++ JSONtoPostgreSQL.js
@@ -1,72 +1,55 @@
+// Imports dontenv module and allows us to access stored environment variables stored in .env file - See https://www.npmjs.com/package/dotenv
+import 'dotenv/config';
 
-/* 
-  Import modules/files you may need to correctly run the script. 
-  Make sure to save your DB's uri in the config file, then import it using `import` statements so that we can ensure we are using ES6 modules 
- */
-  import { Sequelize, Model, DataTypes,  QueryTypes, sql } from '@sequelize/core';
-  
-  //imports dontenv module and allows us to access stored environment variables stored in .env file - See https://www.npmjs.com/package/dotenv
-  import 'dotenv/config';
+// Import file system - Examples of how to use the file system module - fs - https://www.scaler.com/topics/nodejs/fs-module-in-node-js/
+import * as fs from 'fs';
 
-  //Import file system - Examples of how to use the file system module - fs - https://www.scaler.com/topics/nodejs/fs-module-in-node-js/
-  import * as fs from 'fs';
+// Imports the Listing Model we created in ListingModels.js
+import { Listing } from './ListingModel.js';
 
-  //imports the Listing Model we created in ListingModels.js
-  import { Listing } from './ListingModel.js';
+// Testing that the .env file is working - This should print out the port number
+console.log(process.env.PORT); //Should print out 8080
+console.log(process.env.API_Key); //Should print out the API Key
+try {
+    // Setup table in the DB
+    // Read more about Model Synchronization - https://sequelize.org/docs/v6/core-concepts/model-basics/#model-synchronization
+    await Listing.sync({ force: true });
+    console.log("The table for the Listing model was just (re)created!");
 
-/* Connect to your database 
-  See: Sequalize Getting Started - Connecting to a database by passing a URI - Read: https://sequelize.org/docs/v6/getting-started/#connecting-to-a-database
-  Copy URI/URL string from ElephantSQL - under details. 
-  Security Best Practice: Don't use the URL/URI string (postgres://username:password@hostname/databasename) directly in applciation code. Store the database URL as the API_URL environment variable within the .env file. 
-  BAD Implementation - const sequelize = new Sequelize('postgres://user:pass@example.com:5432/dbname') // Example for postgres
-  Good Implementation - const sequelize = new Sequelize(process.env.API_URL);
-  Read - artilce to learn more about environment variables - https://medium.com/the-node-js-collection/making-your-node-js-work-everywhere-with-environment-variables-2da8cdf6e786
-*/
-//ADD CODE HERE to connect to you database
+    // This callback function read the listings.json file into memory (data) and stores errors in (err).
+    // Saves data in Listing variable and displays it on ElephantSQL.
+    fs.readFile('listings.json', 'utf8', function(err, data) {
+        // Throws an error if the data is not available.
+        if (err) throw err
+        {
+            console.log(data);
+        }
 
-//Testing that the .env file is working - This should print out the port number
-console.log(process.env.PORT); //Should print out 8080 
-console.log(process.env.API_Key); //Should print out "Key Not set - starter code only"
+        // Save and parse the data from the listings.json file into a variable, so that we can iterate through each instance - Similar to Bootcamp#1
+        // First parses the data from the JSON file and stores it in listingData variable.
+        // Afterward, turns listingData into an array used to convert data over to ElephantSQL.
+        const listingData=JSON.parse(data)
+        const listingArray = listingData.entries;
 
- try {
-  //Setup table in the DB
-  //Read more about Model Synchronization - https://sequelize.org/docs/v6/core-concepts/model-basics/#model-synchronization
-  await Listing.sync({ force: true });
-  console.log("The table for the Listing model was just (re)created!");
-  
-  /* This callback function read the listings.json file into memory (data) and stores errors in (err).
-      Write code to save the data into the listingData variable and then save each entry into the database.
-   */
-  fs.readFile('listings.json', 'utf8', function(err, data) {
-    // Errors-Check out this resource for an idea of the general format err objects and Throwing an existing object.
-    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/throw#throwing_an_existing_object
-    if (err) throw err
-      {
-          console.log(data);
-      }
-
-    //Save and parse the data from the listings.json file into a variable, so that we can iterate through each instance - Similar to Bootcamp#1
-   //ADD CODE HERE
-      const listingData=JSON.parse(data)
-
-     //Use Sequelize create a new row in our database for each entry in our listings.json file using the Listing model we created in ListingModel.js
-    // to https://sequelize.org/docs/v6/core-concepts/model-instances/#creating-an-instance
-     //ADD CODE HERE
-      (async () => {
-          sequelize.sync({ force: true });
-          // Code here
-          await Listing.create(listingData);
-          // TODO: Stopped here for now.
-      })();
-
+        // Use Sequelize create a new row in our database for each entry in our listings.json file using the Listing model we created in ListingModel.js
+        // to https://sequelize.org/docs/v6/core-concepts/model-instances/#creating-an-instance
+        // Uses a for loop to iterate throughout the listingArray's elements one by one, adding them to the database individually.
+        // Checks if each element has a latitude and longitude via a try and catch statement. If there is none, the array is printed without the optional details.
+        // If there is a latitude and longitude, coordinates are put on the output.
+        for (let i = 0; i < listingArray.length; i++) {
+            try {
+                listingArray[i].latitude = listingArray[i].coordinates.latitude
+                listingArray[i].longitude = listingArray[i].coordinates.longitude
+                Listing.create(listingArray[i])
+            }
+            catch {
+                Listing.create(listingArray[i])
+            }
+        }
     });
-} catch (error) {
-  console.error('Unable to connect to the database:', error);
+}
+catch (error) {
+    console.error('Unable to connect to the database:', error);
 }
 
-
- /* 
-  Once you've written + run the script, check out your ElephantSQL database to ensure that it saved everything correctly. 
- */
-
- 
+// Once you've written + run the script, check out your ElephantSQL database to ensure that it saved everything correctly.
